4fb02429fa5852a7bb7e9d6c80e76ba0f3dd1c33
Lucas Boscaini <lucasboscaini@gmail.com>
2016-12-13 03:21:16 -0200

    Add data vizualization.

--- app/load_dataset.py
+++ app/load_dataset.py
@@ -6,9 +6,13 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 file_path = dir_path + '/../resources/iris.data'
 col_names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 
-dataset = pandas.read_csv(file_path, names=col_names)
+
+def dataset():
+    return pandas.read_csv(file_path, names=col_names)
+
 
 if __name__ == '__main__':
+    dataset = dataset()
     # shape (total rows, total cols)
     print(dataset.shape)
     # head (top N=5 rows)
